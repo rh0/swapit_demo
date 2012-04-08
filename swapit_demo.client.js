0e217a66aa522d8f8fdf0060950798632b6b6524
Ryan Oles <ryan@theoleschool.com>
2012-04-08 00:19:23 -0500

    Adding a bit of ajax to the form to prevent reload. Prosessing message as HTML to compensate for check_plain.

--- swapit_demo.client.js
+++ swapit_demo.client.js
@@ -3,7 +3,7 @@
 Drupal.Nodejs.callbacks.swapIt = {
   //grab the message and inject into the header
   callback: function (message) {
-    $('form #nodejs-selector').text(message.data.body);
+    $('form #nodejs-selector').html(message.data.body);
   }
 };
 
