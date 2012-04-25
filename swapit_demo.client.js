7bc8a365b4078c0c86732a61a69c352751a93c35
Ryan Oles <ryan@theoleschool.com>
2012-04-24 23:10:37 -0500

    adding appropriate conditional

--- swapit_demo.client.js
+++ swapit_demo.client.js
@@ -3,7 +3,7 @@
 Drupal.Nodejs.callbacks.swapIt = {
   //grab the message and inject into the header
   callback: function (message) {
-    if(message.channel = 'swapit_demo') {
+    if(message.channel == 'swapit_demo') {
       $('form #nodejs-selector').html(message.data.body);
     }
   }
